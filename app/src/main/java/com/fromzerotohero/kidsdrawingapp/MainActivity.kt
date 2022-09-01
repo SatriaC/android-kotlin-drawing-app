b81afc3497b1adc24a5c6541191804121f1e858d
catursatria <catursatria006@gmail.com>
2022-09-01 15:14:18 +0700

    share the picture

--- app/src/main/java/com/fromzerotohero/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/fromzerotohero/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -310,11 +311,12 @@ class MainActivity : AppCompatActivity() {
                     //We switch from io to ui thread to show a toast
                     runOnUiThread {
                         cancelProgressDialog()
-                        if (!result.isEmpty()) {
+                        if (result.isNotEmpty()) {
                             Toast.makeText(
                                 this@MainActivity, "File saved successfully :$result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         } else {
                             Toast.makeText(
                                 this@MainActivity,
@@ -342,6 +344,17 @@ class MainActivity : AppCompatActivity() {
         if (customProgressDialog != null) {
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result:String){
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
